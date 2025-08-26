b9abc7b9a71d73b4048e339d45f07fb0433f23c9
AyushG27438 <ayushgg803@gmail.com>
2025-08-27 01:43:17 +0530

    D1Q2.c

--- D1Q2.c
+++ D1Q2.c
@@ -6,19 +6,16 @@ int main() {
     float num1, num2;
     float sum, difference, product, quotient;
 
-    // Input two numbers
     printf("Enter the first number: ");
     scanf("%f", &num1);
 
     printf("Enter the second number: ");
     scanf("%f", &num2);
 
-    // Calculate sum, difference, and product
     sum = num1 + num2;
     difference = num1 - num2;
     product = num1 * num2;
 
-    // Calculate quotient carefully
     if (num2 != 0) {
         quotient = num1 / num2;
         printf("\nResults:\n");
